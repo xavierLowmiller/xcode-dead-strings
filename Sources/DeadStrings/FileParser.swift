2f80966624d6c4c3a7583dd66ed03e70f1c5812d
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-03-20 11:26:23 +0100

    Add tests for actual files

--- Sources/DeadStrings/FileParser.swift
+++ Sources/DeadStrings/FileParser.swift
@@ -7,11 +7,40 @@ func extractStrings(from string: String) -> Set<Substring> {
     var results: Set<Substring> = []
     regex.enumerateMatches(in: string, options: [], range: nsrange) { (match, _, _) in
         guard let match = match,
-        let range = Range(match.range, in: string)
+              let range = Range(match.range, in: string)
         else { return }
 
         results.insert(string[range])
     }
 
     return results
+}
+
+func extractStrings(fromFileAt url: URL) -> Set<Substring> {
+    guard let data = FileManager.default.contents(atPath: url.path) else { return [] }
+    let contents = String(decoding: data, as: UTF8.self)
+    return extractStrings(from: contents)
+}
+
+func extractStrings(fromFilesAt url: URL) -> Set<Substring> {
+    let enumerator = FileManager.default.enumerator(atPath: url.path)
+
+    var strings: Set<Substring> = []
+    while let filename = enumerator?.nextObject() as? String {
+        guard filename.hasSupportedSuffix else { continue }
+        strings.formUnion(extractStrings(fromFileAt: url.appendingPathComponent(filename)))
+    }
+
+    return strings
+}
+
+private extension String {
+    var hasSupportedSuffix: Bool {
+        for suffix in [".swift", ".m", ".mm"] {
+            if hasSuffix(suffix) {
+                return true
+            }
+        }
+        return false
+    }
 }
--- Tests/DeadStringsTests/FileParserTests.swift
+++ Tests/DeadStringsTests/FileParserTests.swift
@@ -70,4 +70,47 @@ final class FileParserTests: XCTestCase {
         XCTAssert(parsedStrings.contains("Cell"))
         XCTAssert(parsedStrings.contains("test"))
     }
+
+    func testExtractingStringsFromFile() {
+        let url = URL(fileURLWithPath: #filePath)
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .appendingPathComponent("MixedObjCProjectForLocalizedString")
+            .appendingPathComponent("MixedObjCProjectForLocalizedString")
+            .appendingPathComponent("AppDelegate.m")
+
+        let strings = extractStrings(fromFileAt: url)
+
+        XCTAssert(strings.contains("Default Configuration"))
+    }
+
+    func testExtractingStringsFromActualProject() {
+        let url = URL(fileURLWithPath: #filePath)
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .appendingPathComponent("MixedObjCProjectForLocalizedString")
+
+        let strings = extractStrings(fromFilesAt: url)
+
+        // ViewController.m
+        XCTAssert(strings.contains("push_first_vc"))
+        XCTAssert(strings.contains("my_view_controller"))
+
+        // SwiftViewController.swift
+        XCTAssert(strings.contains("my_swift_view_controller"))
+        XCTAssert(strings.contains("push_second_vc"))
+
+        // SwiftUIView.swift
+        XCTAssert(strings.contains("login_title"))
+        XCTAssert(strings.contains("mail_address_placeholder"))
+        XCTAssert(strings.contains("password_placeholder"))
+        XCTAssert(strings.contains("swiftui_view_title"))
+
+        // Not localized
+        XCTAssert(strings.contains("secret"))
+        XCTAssert(strings.contains("Default Configuration"))
+
+        // Dead strings
+        XCTAssertFalse(strings.contains("dead_string"))
+    }
 }
